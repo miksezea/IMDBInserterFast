b85e21131b552a5277b266ba776b34bf3084cf05
miksezea <mikkel.eilskov@gmail.com>
2023-10-05 13:15:06 +0200

    Add project files.

---
+++ IMDBConsole/NormalInserter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace IMDBConsole
+{
+    public class NormalInserter
+    {
+        private readonly string ConnString = "server=localhost; database=MyIMDB;" +
+            "user id=sa; password=bibliotek; TrustServerCertificate=True";
+        
+        public void InsertData(List<Title> titles)
+        {
+            SqlConnection sqlConn = new SqlConnection(ConnString);
+            sqlConn.Open();
+
+            foreach (Title title in titles)
+            {
+                SqlCommand sqlCommand = new SqlCommand(
+                    "INSERT INTO [dbo].[Titles]" +
+                    "([tconst],[titleType],[primaryTitle],[originalTitle]," +
+                    "[isAdult],[startYear],[endYear],[runtimeMinutes])" +
+                    "VALUES " +
+                    $"('{title.tconst}','{title.titleType}','{title.primaryTitle.Replace("'", "''")}'," +
+                    $"'{title.originalTitle.Replace("'", "''")}','{title.isAdult}',{CheckIntForNull(title.startYear)}," +
+                    $"{CheckIntForNull(title.endYear)},{CheckIntForNull(title.runtimeMinutes)})", sqlConn);
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(sqlCommand.CommandText);
+                }
+            }
+        }
+
+        public string CheckIntForNull(int? input)
+        {
+            if (input == null)
+            {
+                return "NULL";
+            } else
+            {
+                return "" + input;
+            }
+        }
+    }
+}
---
+++ IMDBConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using IMDBConsole;
+using System.Data.SqlClient;
+
+string ConnString = "server=localhost; database=MyIMDB;" +
+            "user id=sa; password=bibliotek; TrustServerCertificate=True";
+
+List<Title> titles = new List<Title>();
+
+foreach (string line in
+    System.IO.File.ReadLines
+    (@"C:\Users\mikke\OneDrive\Desktop\4Semester\IMDBTSV\title.basics.tsv\data.tsv")
+    .Skip(1).Take(50000))
+{
+    string[] values = line.Split("\t");
+    if (values.Length == 9)
+    {
+        titles.Add(new Title(
+            values[0], values[1], values[2], values[3],
+            ConvertToBool(values[4]), ConvertToInt(values[5]),
+            ConvertToInt(values[6]), ConvertToInt(values[7])
+            ));
+    }
+}
+
+Console.WriteLine(titles.Count);
+
+DateTime before = DateTime.Now;
+
+SqlConnection sqlConn = new SqlConnection(ConnString);
+sqlConn.Open();
+
+IInserter myInserter = new PreparedInserter();
+myInserter.InsertData(sqlConn, titles);
+
+sqlConn.Close();
+
+DateTime after = DateTime.Now;
+
+Console.WriteLine("Tid: " + (after - before));
+
+
+bool ConvertToBool(string input)
+{
+    if (input == "0")
+    {
+        return false;
+    }
+    else if (input == "1")
+    {
+        return true;
+    }
+    throw new ArgumentException(
+        "Kolonne er ikke 0 eller 1, men " + input);
+}
+
+int? ConvertToInt(string input)
+{
+    if (input.ToLower() == @"\n")
+    {
+        return null;
+    }
+    else
+    {
+        return int.Parse(input);
+    }
+
+}
